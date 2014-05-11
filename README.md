6c35996685460e12f54d157ba4161f322b9ac286
m1ga <miga_miga@gmx.de>
2014-05-11 21:35:42 +0200

    download lib

---
+++ README.md
@@ -0,0 +1,20 @@
+== downloader ==
+
+download files to save them or just read them (e.g. json).
+Usage:
+
+require("/download").create({
+	url:"http//:...",download:false
+}).get();
+
+
+=== parameters ===
+- url: string
+- download: boolean
+- overwrite: boolean
+- folder: string
+- success: callback function
+- error: callback function
+- password: string (htaccess)
+- username: string (htaccess)
+- timeout: int
---
+++ downloader.js
@@ -0,0 +1,138 @@
+exports.create = function(opt) {
+	return new Download(opt);
+}
+function Download(opt) {
+
+	var xhr = Ti.Network.createHTTPClient();
+	// switch this off so it will work for all ssl sites
+	xhr.validatesSecureCertificate = false;
+
+
+	var folder = opt.folder || "";
+	var success = opt.success || null;
+	var error = opt.error || null;
+	var progress = opt.progress || null;
+	var timeout = opt.timeout ||5000;
+	var url = opt.url || "";
+	var overwrite = opt.overwrite || false;
+	var dir = Ti.Filesystem.applicationDataDirectory;
+	var download = opt.download || false;
+	var pwd = opt.password || null;
+	var username = opt.username || null;
+
+	if (OS_ANDROID) {
+		dir = Ti.Filesystem.externalStorageDirectory;
+	}
+
+	// check if the folder already exists or create it
+	if (download && !Ti.Filesystem.getFile(dir, folder).exists()) {
+		Ti.Filesystem.getFile(dir, folder).createDirectory();
+	}
+
+
+	this.get = function() {
+
+		var fname = url.substring(url.lastIndexOf('/') + 1);
+
+		fname = fname.replace(/\?/g, '');
+		fname = fname.replace(/=/g, '');
+
+		var f = Ti.Filesystem.getFile(dir, folder, fname);
+
+		if (!download || (download && overwrite) || (!f.exists() && fname != "null")) {
+
+			Ti.API.info("get: " + url);
+
+			xhr.ondatastream = function(e) {
+				if (xhr.getResponseHeader('Content-Length')) {
+					var full = xhr.getResponseHeader('Content-Length') / 1024 / 1024;
+					if (progress) progress({progress:e.progress});
+					full=null;
+				}
+			}
+
+			xhr.onload = function() {
+				if (this.readyState == 4) {
+					var content = null;
+					Ti.API.info("done");
+					if (download) {
+						// save file
+						var f = Ti.Filesystem.getFile(dir, folder, fname);
+						f.write(this.responseData);
+					} else {
+						// give back the content of the file
+						content = this.responseData;
+					}
+
+					// call success function
+					if (success) success({
+						url: url, file:fname,content:content
+					});
+
+					// clean up
+					f = null;
+					content = null;
+					cleanUp();
+				} else {
+					// call error function
+					if (error) error({
+						url: url, file:fname
+					});
+
+					// clean up
+					f = null;
+					cleanUp();
+					return false;
+				}
+			}
+			xhr.onerror = function(e) {
+				Ti.API.info("download error " + JSON.stringify(e));
+				if (error) error({
+					url: url, file:fname
+				});
+				cleanUp();
+				return false;
+			}
+
+			xhr.timeout = timeout;
+			xhr.open('GET', url);
+			if (pwd != "") {
+				// set password
+				var authstr = 'Basic ' + Titanium.Utils.base64encode(username+":"+pwd);
+				authstr = authstr.replace(/(\r\n|\n|\r)/gm, "");
+				xhr.setRequestHeader('Authorization', authstr);
+				authstr = null;
+			}
+			xhr.send();
+
+		} else {
+			// already there
+			Ti.API.info("skipping " + url);
+			if (success) success({
+				url: url, file:fname
+			});
+			cleanUp();
+		}
+	}
+
+	function cleanUp(){
+		xhr = null;
+		fname = null;
+		error = null;
+		url = null;
+		success = null;
+		progress = null;
+		timeout = null;
+		folder = null;
+		overwrite = null;
+		dir = null;
+		download= null;
+		pwd =null;
+		username=null;
+	}
+
+	this.abort = function() {
+		xhr.abort();
+		cleanUp();
+	}
+}
